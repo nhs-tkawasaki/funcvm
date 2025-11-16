dbcd64d84c04dbabf2a4dc3503e48ed83322d004
kawasaki.taiga <kawasaki.taiga.r1z@nhs.co.jp>
2025-11-17 02:10:45 +0900

    simplify

--- proxy-fetch.js
+++ proxy-fetch.js
@@ -1,17 +1,8 @@
 const fetch = require('node-fetch');
 const { ProxyAgent } = require('proxy-agent');
 
-function fetchWithProxy(url, options = {}) {
-    if (options.agent) {
-        return fetch(url, options);
-    }
-
-    const proxiedOptions = {
-        ...options,
-        agent: new ProxyAgent(),
-    };
-
-    return fetch(url, proxiedOptions);
+function fetchWithProxy(url) {
+    return fetch(url, { agent: new ProxyAgent() });
 }
 
 module.exports = fetchWithProxy;
